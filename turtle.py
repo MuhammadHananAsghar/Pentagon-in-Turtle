956605c70e4b927b31918cda41ef9494d15b55ac
Muhammad Hanan Asghar <44013285+MuhammadHananAsghar@users.noreply.github.com>
2020-07-15 01:03:53 +0545

    Pentagon

---
+++ turtle.py
@@ -0,0 +1,22 @@
+import turtle
+from time import sleep
+
+wm = turtle.Screen()
+wm.bgcolor("lightgreen")
+
+pent = turtle.Turtle()
+
+sleep(1)
+pent.color("white")
+pent.pensize(3)
+pent.left(90)
+pent.forward(70)
+pent.right(45)
+pent.forward(70)
+pent.right(90)
+pent.forward(70)
+pent.right(45)
+pent.forward(70)
+pent.right(90)
+pent.forward(100)
+sleep(2)
